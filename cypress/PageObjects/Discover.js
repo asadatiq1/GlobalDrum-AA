fef1872b04749f084f967b233d5d4efe911b1fda
asadatiq1 <asadatiq5@gmail.com>
2023-04-13 12:19:29 +0500

    initial

---
+++ cypress/PageObjects/Discover.js
@@ -0,0 +1,3 @@
+class discover {}
+
+export default discover;
---
+++ cypress/e2e/OpenApplication.cy.js
@@ -0,0 +1,11 @@
+describe("template spec", () => {
+  // beforeEach(() => {
+  //   cy.clearCookies()
+  //   // cy.viewport(375,812)
+  //   cy.visit("https://www.aa-stage.global-drum.com");
+  // });
+  it("passes", () => {
+    cy.visit("https://www.aa-stage.global-drum.com/discover");
+  });
+ 
+});
