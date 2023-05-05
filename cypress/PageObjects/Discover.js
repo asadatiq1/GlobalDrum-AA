164935a7b96a831deb6693c8ed0b92b484f7b329
asadatiq1 <asadatiq5@gmail.com>
2023-05-05 12:42:51 +0500

    GD-2

--- cypress/PageObjects/Discover.js
+++ cypress/PageObjects/Discover.js
@@ -1,9 +1,19 @@
 class discover {
   // Xpaths:
+  splash_screen ="div[class='inner-swipe-up-wrapper'] h6";
   header = "li[class='single-tab single-tab--active'] button[class='tab-btn']";
   // Methods:
+  verify_splashScreen(){
+    cy.get(this.splash_screen).should('have.text','Swipe up to Start');
+  }
   verify_page(){
     cy.get(this.header).should('have.text','Discover');
+  }
+  swipeup_SplashScreen(){
+    cy.get(this.splash_screen)
+   .trigger('pointerdown', { which: 1 })
+   .trigger('pointermove', 'up')
+   .trigger('pointerup', { force: true })
   }
 }
 
--- cypress/e2e/GuestUserDiscover.cy.js
+++ cypress/e2e/GuestUserDiscover.cy.js
@@ -1,19 +1,17 @@
 import discover from "../PageObjects/Discover";
 
-describe("Open Application Flows", () => {
+describe("Guest User Flows", () => {
   // PageObjects
   const ds = new discover();
   // Runs
   beforeEach(() => {
-    cy.clearCookies()
-    cy.viewport(375,812)
+    cy.clearCookies();
+    cy.viewport(375, 812);
     cy.visit("https://www.aa-stage.global-drum.com");
   });
-  // Test Cases
-  it("Landing-Guest-User", () => {
-    ds.verify_splashScreen();
-    ds.swipeup_SplashScreen();
+  //   Test Cases
+  it("Landing-Page", () => {
     ds.verify_page();
   });
- 
+  it ("")
 });
